06e8df7863d76ca48c7f74da071213c241560771
Kaique araujo lopes <kaiquearaujo246@hotmail.com>
2025-11-20 20:13:14 -0300

    feature: concluding a session on color themes

--- src/components/pages/dashboard/resume/structure-sidebar/index.tsx
+++ src/components/pages/dashboard/resume/structure-sidebar/index.tsx
@@ -1,15 +1,20 @@
 import { Separator } from "@/components/ui/separator";
 import { TemplatesListSection } from "./sections/templates-list";
 import { LayoutSection } from "./sections/layout";
+import { ThemeSection } from "./sections/theme";
 
 export function StructureSidebar() {
-    return(
+    return (
         <aside className="w-full h-full p-6 overflow-y-auto">
             <TemplatesListSection />
 
-            <Separator className="my-5"/>
+            <Separator className="my-5" />
 
             <LayoutSection />
+
+            <Separator className="my-5" />
+
+            <ThemeSection />
         </aside>
     )
 }
---
+++ src/components/pages/dashboard/resume/structure-sidebar/sections/theme.tsx
@@ -0,0 +1,62 @@
+import { Palette } from "lucide-react";
+import { SectionTitle } from "../../infos-sidebar/section-title";
+
+import colors from "tailwindcss/colors";
+import { Controller, useForm, useFormContext } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+
+const keysToIgnore = [
+    "current",
+    "inherit",
+    "currentColor",
+    "transparent",
+    "black",
+    "white",
+];
+
+const colorKeys = Object.keys(colors).filter((key) => !keysToIgnore.includes(key)) as (keyof typeof colors)[];
+
+export function ThemeSection() {
+    const { control } = useFormContext<ResumeData>();
+
+    return (
+        <div>
+            <SectionTitle
+                title="Tema"
+                icon={Palette}
+            />
+
+            <Controller
+                control={control}
+                name="structure.colorTheme"
+                render={({ field }) => (
+                    <div className="grid grid-cols-7 gap-4 mt-4">
+                        {
+                            colorKeys.map(colorKeys => {
+                                const isSelected = field.value === colorKeys;
+
+                                return (
+                                    <Button
+                                        key={colorKeys}
+                                        variant="ghost"
+                                        className={cn(
+                                            "w-7 h-7 p-1 rounded-full transition-all",
+                                            isSelected && "ring-2 ring-foreground"
+                                        )}
+                                        onClick={() => field.onChange(colorKeys)}
+                                    >
+                                        <div
+                                            className="w-full h-full rounded-full"
+                                            style={{ background: colors[colorKeys][500] }}
+                                        />
+                                    </Button>
+                                )
+                            })
+                        }
+                    </div>
+                )}
+            />
+        </div>
+    )
+}
