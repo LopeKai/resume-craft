de4b73418562eb5e8db97e03221dbe4e46631738
Kaique araujo lopes <kaiquearaujo246@hotmail.com>
2025-11-22 17:06:22 -0300

    configuring the react-zoom-pan-pinch libray

--- src/components/pages/dashboard/resume/resume-content/index.tsx
+++ src/components/pages/dashboard/resume/resume-content/index.tsx
@@ -1,9 +1,32 @@
+"use client";
+
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+import { ResumeTemplate } from "./templates";
+import { TransformControls } from "./controls";
+import { NavigationHeader } from "./header";
+
 export function ResumeContent() {
     return (
-        <section 
+        <section
             className="overflow-hidden w-full h-full flex items-center justify-center relative bg-muted dark:bg-background"
         >
-            ola
+            <TransformWrapper
+                initialScale={0.5}
+                minScale={0.4}
+                centerOnInit
+                centerZoomedOut
+                limitToBounds={false}
+            >
+                <>
+                    <NavigationHeader />
+
+                    <TransformControls />
+
+                    <TransformComponent>
+                        <ResumeTemplate />
+                    </TransformComponent>
+                </>
+            </TransformWrapper>
         </section>
     )
 }
---
+++ src/components/pages/dashboard/resume/resume-content/templates/index.tsx
@@ -0,0 +1,10 @@
+export function ResumeTemplate() {
+    return (
+        <div
+            id="resume-content"
+            className="w-[210mm] min-h-[297mm] bg-white text-black font-arial [&_hr]:border-black"
+        >
+            
+        </div>
+    )
+}
